1eaf48e95537c04ceef51470e8445599375aefab
Jibon <manvirjibon@gmail.com>
2023-12-22 11:34:29 +0600

    post done

--- js/index.js
+++ js/index.js
@@ -2,29 +2,26 @@ let inptName =document.querySelector(".inptName");
 let inputCaption =document.querySelector('.inputCaption');
 let postBtn =document.querySelector('.postBtn');
 let allPost =document.querySelector(".allPost")
-let arr=[{
-    name:"jibon",
-  },{
-    name:"ddd",
-  },{
-  
-    name:"vvv",
-  },]
+let arr=[]
 postBtn.addEventListener("click",function(){
-    console.log(inptName.value);
-    console.log(inputCaption.value);
+    allPost.innerHTML=""
+   arr.push({
+    name:inptName.value,
+    caption:inputCaption.value,
+   })
+   Display()
 })
 function Display(){
     arr.map((item)=>{
         allPost.innerHTML += `<div class="card" style="width: 18rem; margin-top: 30px;">
         <div class="card-body">
           <h5 class="card-title">${item.name}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">${item.caption}</p>
           <button type="button" class="btn btn-primary">Edit</button>
           <button type="button" class="btn btn-danger">Delet</button>
         </div>
       </div>`
     })
 }
-Display()
+
 
