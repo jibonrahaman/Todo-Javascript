20006a75dfc6c4c88b2c59ef5d6aa7daa4ae7a45
Jibon <manvirjibon@gmail.com>
2023-12-22 11:28:17 +0600

    card setup function method

--- js/index.js
+++ js/index.js
@@ -2,15 +2,29 @@ let inptName =document.querySelector(".inptName");
 let inputCaption =document.querySelector('.inputCaption');
 let postBtn =document.querySelector('.postBtn');
 let allPost =document.querySelector(".allPost")
-let arr=[]
+let arr=[{
+    name:"jibon",
+  },{
+    name:"ddd",
+  },{
+  
+    name:"vvv",
+  },]
 postBtn.addEventListener("click",function(){
     console.log(inptName.value);
     console.log(inputCaption.value);
 })
 function Display(){
- arr.map((item)=>{
-    
- })
+    arr.map((item)=>{
+        allPost.innerHTML += `<div class="card" style="width: 18rem; margin-top: 30px;">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button type="button" class="btn btn-primary">Edit</button>
+          <button type="button" class="btn btn-danger">Delet</button>
+        </div>
+      </div>`
+    })
 }
 Display()
 
