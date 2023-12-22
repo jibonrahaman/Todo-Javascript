64233f504e10069f1e38d205a6ee7a3d37b4e763
Jibon <manvirjibon@gmail.com>
2023-12-22 11:14:38 +0600

    fucntion call

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 let inptName =document.querySelector(".inptName");
 let inputCaption =document.querySelector('.inputCaption');
 let postBtn =document.querySelector('.postBtn');
-
-
+let allPost =document.querySelector(".allPost")
+let arr=[]
 postBtn.addEventListener("click",function(){
     console.log(inptName.value);
     console.log(inputCaption.value);
 })
+function Display(){
+ arr.map((item)=>{
+    
+ })
+}
+Display()
 
